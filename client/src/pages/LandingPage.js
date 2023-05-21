0e0e43ded2c329155e8bdd9afd27a7aa2b3a2fcd
Taisir <taisirisawesome@gmail.com>
2023-05-20 20:35:48 -0400

    added colouring changes

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,58 +1,76 @@
 import React from "react";
 import "../styles/LandingPage.css";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <nav id="navbar">
+      <nav className="navbar">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/app">App</a></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/app">App</Link>
+          </li>
         </ul>
       </nav>
-      <header>
-        <h1>Welcome to Handshake</h1>
-        <p id="subtitle">Streamline contract agreements and automate secure fund transfers.</p>
-      </header>
 
-      <section>
+      <div className="content-section">
+        <header>
+          <h1>Welcome to Handshake</h1>
+          <p id="subtitle">
+            Streamline contract agreements and automate secure fund transfers.
+          </p>
+        </header>
 
-        <img id="collab_img" src="https://plus.unsplash.com/premium_photo-1663956045546-80dd104c018f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"></img>
-        <span id="img_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span>
-      </section>
-      <section className="features">
-        <div className="feature">
-          <h2>Decentralized Marketplace</h2>
+        <section>
+          <img
+            id="collab_img"
+            src="https://plus.unsplash.com/premium_photo-1663956045546-80dd104c018f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"
+            alt="Collaboration"
+          />
+          <span id="img_text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          </span>
+        </section>
+
+        <section className="features">
+          <div className="feature">
+            <h2>Decentralized Marketplace</h2>
+            <p>
+              Explore a diverse collection of music from talented independent
+              artists.
+            </p>
+          </div>
+          <div className="feature">
+            <h2>Royalty Automation</h2>
+            <p>
+              Smart contracts ensure fair and transparent royalty distribution.
+            </p>
+          </div>
+          <div className="feature">
+            <h2>Community Engagement</h2>
+            <p>
+              Connect with fellow music enthusiasts and artists in our vibrant
+              community.
+            </p>
+          </div>
+        </section>
+
+        <section className="cta-section">
+          <h2>Join MusicChain Today</h2>
           <p>
-            Explore a diverse collection of music from talented independent
+            Sign up to start exploring, purchasing, and supporting your favorite
             artists.
           </p>
-        </div>
-        <div className="feature">
-          <h2>Royalty Automation</h2>
-          <p>
-            Smart contracts ensure fair and transparent royalty distribution.
-          </p>
-        </div>
-        <div className="feature">
-          <h2>Community Engagement</h2>
-          <p>
-            Connect with fellow music enthusiasts and artists in our vibrant
-            community.
-          </p>
-        </div>
-      </section>
-      <section className="cta-section">
-        <h2>Join MusicChain Today</h2>
-        <p>
-          Sign up to start exploring, purchasing, and supporting your favorite
-          artists.
-        </p>
-        <button className="cta-button">Get Started</button>
-      </section>
-      <footer>
-        <p>&copy; 2023 MusicChain. All rights reserved.</p>
-      </footer>
+          <button className="cta-button">Get Started</button>
+        </section>
+
+        <footer>
+          <p>&copy; 2023 Handshake. All rights reserved.</p>
+        </footer>
+      </div>
     </div>
   );
 };
--- client/src/pages/Smart.js
+++ client/src/pages/Smart.js
@@ -2,142 +2,145 @@ import React, { useState } from "react";
 import "../styles/Smart.css";
 
 const Smart = () => {
-    const [partner, setPartner] = useState("");
-    const [walletAddress, setWalletAddress] = useState("");
-    const [amount, setAmount] = useState("");
-    const [coinType, setCoinType] = useState("");
-    const [securityRequirements, setSecurityRequirements] = useState("");
-    const [contractDuration, setContractDuration] = useState("");
+  const [partner, setPartner] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
+  const [amount, setAmount] = useState("");
+  const [coinType, setCoinType] = useState("");
+  const [securityRequirements, setSecurityRequirements] = useState("");
+  const [contractDuration, setContractDuration] = useState("");
 
-    const handlePartnerChange = (event) => {
-        setPartner(event.target.value);
-    };
+  const handlePartnerChange = (event) => {
+    setPartner(event.target.value);
+  };
 
-    const handleWalletAddressChange = (event) => {
-        setWalletAddress(event.target.value);
-    };
+  const handleWalletAddressChange = (event) => {
+    setWalletAddress(event.target.value);
+  };
 
-    const handleAmountChange = (event) => {
-        setAmount(event.target.value);
-    };
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
 
-    const handleCoinTypeChange = (event) => {
-        setCoinType(event.target.value);
-    };
+  const handleCoinTypeChange = (event) => {
+    setCoinType(event.target.value);
+  };
 
-    const handleSecurityRequirementsChange = (event) => {
-        setSecurityRequirements(event.target.value);
-    };
+  const handleSecurityRequirementsChange = (event) => {
+    setSecurityRequirements(event.target.value);
+  };
 
-    const handleContractDurationChange = (event) => {
-        setContractDuration(event.target.value);
-    };
+  const handleContractDurationChange = (event) => {
+    setContractDuration(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        // Perform any necessary actions with the form data
-        console.log({
-            partner,
-            walletAddress,
-            amount,
-            coinType,
-            securityRequirements,
-            contractDuration,
-        });
+    // Perform any necessary actions with the form data
+    console.log({
+      partner,
+      walletAddress,
+      amount,
+      coinType,
+      securityRequirements,
+      contractDuration,
+    });
 
-        // Reset the form fields
-        setPartner("");
-        setWalletAddress("");
-        setAmount("");
-        setCoinType("");
-        setSecurityRequirements("");
-        setContractDuration("");
-    };
+    // Reset the form fields
+    setPartner("");
+    setWalletAddress("");
+    setAmount("");
+    setCoinType("");
+    setSecurityRequirements("");
+    setContractDuration("");
+  };
 
-    return (
-        <div className="smart">
+  return (
+    <div className="smart">
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/app">App</a>
+          </li>
+        </ul>
+      </nav>
 
-            <nav id="navbar">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/app">App</a></li>
-                </ul>
-            </nav>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="partner-select">
+          Select Partner:
+          <select
+            name="partner"
+            id="partner-select"
+            value={partner}
+            onChange={handlePartnerChange}
+          >
+            <option value="partner1">Partner 1</option>
+            <option value="partner2">Partner 2</option>
+            <option value="partner3">Partner 3</option>
+          </select>
+        </label>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="partner-select">
-                    Select Partner:
-                    <select
-                        name="partner"
-                        id="partner-select"
-                        value={partner}
-                        onChange={handlePartnerChange}
-                    >
-                        <option value="partner1">Partner 1</option>
-                        <option value="partner2">Partner 2</option>
-                        <option value="partner3">Partner 3</option>
-                    </select>
-                </label>
+        <label htmlFor="wallet-address-input">
+          Wallet Address:
+          <input
+            type="text"
+            id="wallet-address-input"
+            value={walletAddress}
+            onChange={handleWalletAddressChange}
+          />
+        </label>
 
-                <label htmlFor="wallet-address-input">
-                    Wallet Address:
-                    <input
-                        type="text"
-                        id="wallet-address-input"
-                        value={walletAddress}
-                        onChange={handleWalletAddressChange}
-                    />
-                </label>
+        <label htmlFor="amount-input">
+          Amount:
+          <input
+            type="text"
+            id="amount-input"
+            value={amount}
+            onChange={handleAmountChange}
+          />
+        </label>
 
-                <label htmlFor="amount-input">
-                    Amount:
-                    <input
-                        type="text"
-                        id="amount-input"
-                        value={amount}
-                        onChange={handleAmountChange}
-                    />
-                </label>
+        <label htmlFor="coin-type-input">
+          Coin Type:
+          <input
+            type="text"
+            id="coin-type-input"
+            value={coinType}
+            onChange={handleCoinTypeChange}
+          />
+        </label>
 
-                <label htmlFor="coin-type-input">
-                    Coin Type:
-                    <input
-                        type="text"
-                        id="coin-type-input"
-                        value={coinType}
-                        onChange={handleCoinTypeChange}
-                    />
-                </label>
+        <label htmlFor="security-requirements-input">
+          Security Requirements:
+          <input
+            type="text"
+            id="security-requirements-input"
+            value={securityRequirements}
+            onChange={handleSecurityRequirementsChange}
+          />
+        </label>
 
-                <label htmlFor="security-requirements-input">
-                    Security Requirements:
-                    <input
-                        type="text"
-                        id="security-requirements-input"
-                        value={securityRequirements}
-                        onChange={handleSecurityRequirementsChange}
-                    />
-                </label>
+        <label htmlFor="contract-duration-select">
+          Contract Duration:
+          <select
+            name="contract-duration"
+            id="contract-duration-select"
+            value={contractDuration}
+            onChange={handleContractDurationChange}
+          >
+            <option value="1">1 day</option>
+            <option value="7">7 days</option>
+            <option value="30">30 days</option>
+          </select>
+        </label>
 
-                <label htmlFor="contract-duration-select">
-                    Contract Duration:
-                    <select
-                        name="contract-duration"
-                        id="contract-duration-select"
-                        value={contractDuration}
-                        onChange={handleContractDurationChange}
-                    >
-                        <option value="1">1 day</option>
-                        <option value="7">7 days</option>
-                        <option value="30">30 days</option>
-                    </select>
-                </label>
-
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Smart;
